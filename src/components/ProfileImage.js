ad4f0645ae34dae89f3cb9d92fb6254ba896288c
akag0d <ruben.neves90@gmail.com>
2022-02-28 22:25:03 +0000

    done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ProfileImage(props) {
+  return (
+      <div>
+    <img
+    src="https://i.imgur.com/b0EdHVV.jpg"
+    className="profile"
+    alt="profile"
+  />
+    </div>
+  )
+}
+
+export default ProfileImage;
